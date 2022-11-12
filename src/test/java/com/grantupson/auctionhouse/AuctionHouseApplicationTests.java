981a280e5964a93b8ecf9b713cfb26acbf0b2bc1
Grant Upson <grant@grantupson.com>
2022-11-13 00:11:38 +1100

    Created initial project

--- src/test/java/com/grantupson/auctionhouse/AuctionHouseApplicationTests.java
+++ src/test/java/com/grantupson/auctionhouse/AuctionHouseApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AuctionHouseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
